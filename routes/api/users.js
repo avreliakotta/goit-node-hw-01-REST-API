b59ad16a12b08394b6162af899953bae73fa8d24
Nataliia Melnyk <natalia.melnyk28@gmail.com>
2023-11-30 13:30:19 +0200

    Added  multer

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,18 @@
-const express = require ("express");
-const { validateBody,authenticate } = require("../../middleware");
-const {schemas} = require("../../models/mongooseModel/user");
-const ctrl=require("../../controllers/auth");
+const express = require('express');
+const { validateBody, authenticate, upload } = require('../../middleware');
+const { schemas } = require('../../models/mongooseModel/user');
+const ctrl = require('../../controllers/auth');
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema),ctrl.register);
+router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 
-router.post("/login",validateBody(schemas.loginSchema),ctrl.login);
+router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 
-router.get("/current",authenticate,ctrl.getCurrent);
+router.get('/current', authenticate, ctrl.getCurrent);
 
-router.post("/logout",authenticate,ctrl.logout);
+router.post('/logout', authenticate, ctrl.logout);
 
-module.exports=router;
+router.patch('/avatars', authenticate, upload.single('avatar'),ctrl.updateAvatar);
+
+module.exports = router;
